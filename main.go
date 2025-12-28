afa0aae0951324444f5e48668de5c5498ac2a332
kai-xlr <kahlil.wallace@protonmail.com>
2025-12-27 23:44:50 -0500

    Rollback to fix issues

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"sync/atomic"
 )
 
 const (
@@ -12,39 +11,13 @@ const (
 	filepathRoot = "."
 )
 
-type apiConfig struct {
-	fileserverHits atomic.Int32
-}
-
-func (a *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		a.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (a *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", a.fileserverHits.Load())))
-}
-
 func main() {
-	apiCfg := &apiConfig{}
-
 	mux := http.NewServeMux()
 
 	fs := http.FileServer(http.Dir(filepathRoot))
 	mux.Handle("/", fs)
-
-	appHandler := apiCfg.middlewareMetricsInc(
-		http.StripPrefix("/app", fs),
-	)
-	mux.Handle("/app/", appHandler)
-
+	mux.Handle("/app/", http.StripPrefix("/app", fs))
 	mux.HandleFunc("/healthz", handlerReadiness)
-	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
@@ -55,4 +28,9 @@ func main() {
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatal(err)
 	}
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
--- readiness.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "net/http"
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
